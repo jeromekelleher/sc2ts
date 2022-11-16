fe31318dd80a1fad18d81316804c0d501ff0f30d
Jerome Kelleher <jk@well.ox.ac.uk>
2022-11-16 11:45:20 +0000

    Move data into package

--- get_problematic_sites.sh
+++ get_problematic_sites.sh
@@ -4,4 +4,4 @@
 
 rm -f problematic_sites_sarsCov2.vcf
 wget https://raw.githubusercontent.com/W-L/ProblematicSites_SARS-CoV2/master/problematic_sites_sarsCov2.vcf
-bcftools view -H problematic_sites_sarsCov2.vcf | cut -f2 > problematic_sites.txt
+bcftools view -H problematic_sites_sarsCov2.vcf | cut -f2 > sarscov2ts/data/problematic_sites.txt
