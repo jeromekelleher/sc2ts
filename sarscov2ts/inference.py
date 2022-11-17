247bd5d0a30ad69476705cb193527fcc65cf25a1
Jerome Kelleher <jk@well.ox.ac.uk>
2022-11-17 10:34:37 +0000

    Moved tsinfer "Extend" operation into the code here.

--- sarscov2ts/inference.py
+++ sarscov2ts/inference.py
@@ -1,9 +1,16 @@
 import logging
 import datetime
+import dataclasses
+import collections
 
 import tqdm
+import tskit
 import tsinfer
 import numpy as np
+
+from . import constants
+
+logger = logging.getLogger(__name__)
 
 
 def infer(
@@ -51,19 +58,462 @@ def infer(
     num_rejected = sd.num_samples - num_samples
     fraction_rejected = num_rejected / sd.num_samples
 
-    logging.info(
+    logger.info(
         f"Filtered {num_rejected} ({100 * fraction_rejected:.2f}%) samples "
         f"with submission_delay > {max_submission_delay}"
     )
-    logging.info(f"Extending for {current_date} with {len(samples)} samples")
+    logger.info(f"Extending for {current_date} with {len(samples)} samples")
 
-    extender = tsinfer.SequentialExtender(
-        sd, ancestors_ts=ancestors_ts, time_units="days_ago"
-    )
+    extender = SequentialExtender(sd, ancestors_ts=ancestors_ts, time_units="days_ago")
     ts = extender.extend(
         samples, num_mismatches=num_mismatches, time_increment=increment, **kwargs
     )
     return ts
+
+
+def solve_num_mismatches(ts, k):
+    """
+    Return the low-level LS parameters corresponding to accepting
+    k mismatches in favour of a single recombination.
+    """
+    m = ts.num_sites
+    n = ts.num_nodes  # We can match against any node in tsinfer
+    if k == 0:
+        # Pathological things happen when k=0
+        r = 1e-3
+        mu = 1e-20
+    else:
+        mu = 1e-6
+        denom = (1 - mu) ** k + (n - 1) * mu**k
+        r = n * mu**k / denom
+        assert mu < 0.5
+        assert r < 0.5
+
+    ls_recomb = np.full(m - 1, r)
+    ls_mismatch = np.full(m, mu)
+    return ls_recomb, ls_mismatch
+
+
+class Matcher(tsinfer.SampleMatcher):
+    """
+    NOTE: this is using undocumented internal APIs as a way of accessing
+    tsinfer's Li and Stephens matching engine. There are some awkward
+    workaround involved in dealing with tsinfer's internal representation
+    of the data, which are tightly coupled to implementation details within
+    tsinfer.
+
+    This implementation will be swapped out for tskit's LS engine in the
+    near future, using fully documented and supported APIs.
+    """
+
+    def extend(self, samples, node_metadata):
+        """
+        Runs the "extend" operation matching a set of samples in the existing
+        tree, returning a new tree sequence.
+
+        NOTE: this is not part of the public API is likely to be removed
+        once sc2ts moves over to using the tskit haplotype matching LS engine.
+        """
+        builder = self.tree_sequence_builder
+        # Allocate nodes in the tree sequence consecutively for the input samples
+        for sd_id in samples:
+            self.sample_id_map[sd_id] = builder.add_node(0)
+
+        self._match_samples(samples)
+
+        tsb = self.tree_sequence_builder
+        tables = self.ancestors_ts_tables.copy()
+        tables.edges.clear()
+
+        logger.debug("Adding tree sequence nodes")
+        flags, node_time = tsb.dump_nodes()
+
+        # First add the sample nodes for *all* the input samples
+        for sd_id in samples:
+            tables.nodes.add_row(
+                flags=tskit.NODE_IS_SAMPLE, time=0, metadata=node_metadata[sd_id]
+            )
+        for u in range(len(tables.nodes), tsb.num_nodes):
+            tables.nodes.add_row(flags=flags[u], time=node_time[u])
+
+        logger.debug("Adding tree sequence edges")
+        left, right, parent, child = tsb.dump_edges()
+        tables.edges.append_columns(
+            left=self.position_map[left],
+            right=self.position_map[right],
+            parent=parent,
+            child=child,
+        )
+
+        logger.debug("Sorting and building intermediate tree sequence.")
+        tables.sites.clear()
+        old_num_mutations = len(tables.mutations)
+        tables.mutations.clear()
+        tables.sort()
+        tables.build_index()
+
+        mut_site, node, derived_state, _ = self.tree_sequence_builder.dump_mutations()
+        mutation_id = 0
+        num_mutations = len(mut_site)
+        for site in self.sample_data.sites(self.inference_site_id):
+            site_id = tables.sites.add_row(
+                site.position,
+                ancestral_state=site.alleles[0],
+            )
+            while mutation_id < num_mutations and mut_site[mutation_id] == site_id:
+                tables.mutations.add_row(
+                    site_id,
+                    node=node[mutation_id],
+                    derived_state=site.alleles[derived_state[mutation_id]],
+                    time=node_time[node[mutation_id]],
+                )
+                mutation_id += 1
+
+        tables.compute_mutation_parents()
+        new_mutations = num_mutations - old_num_mutations
+        logger.info(
+            f"Extended for {len(samples)} samples and "
+            f"{new_mutations} new mutations."
+        )
+        return tables.tree_sequence()
+
+
+class SequentialExtender:
+    def __init__(self, sample_data, ancestors_ts=None, time_units=None):
+        time_units = tskit.TIME_UNITS_UNCALIBRATED if time_units is None else time_units
+        self.sample_data = sample_data
+        if ancestors_ts is None:
+            tables = tskit.TableCollection(sample_data.sequence_length)
+            tables.time_units = time_units
+            for site in sample_data.sites():
+                tables.sites.add_row(site.position, site.ancestral_state)
+            tables.nodes.metadata_schema = tskit.MetadataSchema.permissive_json()
+            # TODO should probably make the ultimate ancestor time something less
+            # plausible or at least configurable.
+            for t in [1, 0]:
+                tables.nodes.add_row(time=t)
+            tables.edges.add_row(0, sample_data.sequence_length, 0, 1)
+            self.ancestors_ts = tables.tree_sequence()
+        else:
+            self.ancestors_ts = ancestors_ts
+            if time_units is not None and self.ancestors_ts.time_units != time_units:
+                raise ValueError(
+                    f"Mismatched time_units: {time_units} != ancestors_ts.time_units",
+                )
+
+        self.node_metadata = sample_data.individuals_metadata[:]
+        assert self.sample_data.num_individuals == self.sample_data.num_samples
+
+    def _increment_ancestors_ts_time(self, increment):
+        tables = self.ancestors_ts.dump_tables()
+        tables.nodes.time += increment
+        tables.mutations.time += increment
+        self.ancestors_ts = tables.tree_sequence()
+
+    def extend(self, samples, num_mismatches=None, time_increment=None, **kwargs):
+        num_mismatches = 0 if num_mismatches is None else num_mismatches
+        time_increment = 1 if time_increment is None else time_increment
+
+        ls_recomb, ls_mismatch = solve_num_mismatches(self.ancestors_ts, num_mismatches)
+        self._increment_ancestors_ts_time(time_increment)
+        manager = Matcher(
+            self.sample_data,
+            self.ancestors_ts,
+            allow_multiallele=True,
+            recombination=ls_recomb,
+            mismatch=ls_mismatch,
+            **kwargs,
+        )
+        ts = manager.extend(np.array(samples), self.node_metadata)
+        ts = coalesce_mutations(ts)
+        ts = push_up_reversions(ts)
+        self.ancestors_ts = ts
+        return ts
+
+
+@dataclasses.dataclass(frozen=True)
+class MutationDescriptor:
+    site: float
+    derived_state: str
+    inherited_state: str
+    parent: int
+
+    def is_reversion_of(self, other) -> bool:
+        """
+        Returns True if this mutation is a reversion of the other.
+        """
+        assert self.site == other.site
+        return self.derived_state == other.inherited_state
+
+
+def node_mutation_descriptors(ts, u):
+    descriptors = set()
+    for mut_id in np.where(ts.mutations_node == u)[0]:
+        mut = ts.mutation(mut_id)
+        inherited_state = ts.site(mut.site).ancestral_state
+        if mut.parent != -1:
+            parent_mut = ts.mutation(mut.parent)
+            if parent_mut.node == u:
+                raise ValueError("Multiple mutations on same branch not supported")
+            inherited_state = parent_mut.derived_state
+        descriptors.add(
+            MutationDescriptor(mut.site, mut.derived_state, inherited_state, mut.parent)
+        )
+    return descriptors
+
+
+def update_tables(tables, edges_to_delete, mutations_to_delete):
+
+    # Updating the mutations is a real faff, and the only way I
+    # could get it to work is by setting the time values. This should
+    # be easier...
+    mutations_to_keep = np.ones(len(tables.mutations), dtype=bool)
+    mutations_to_keep[mutations_to_delete] = False
+    tables.mutations.replace_with(tables.mutations[mutations_to_keep])
+    # Set the parent values to -1 and recompute them later.
+    tables.mutations.parent = np.full_like(tables.mutations.parent, -1)
+
+    edges_to_keep = np.ones(len(tables.edges), dtype=bool)
+    edges_to_keep[edges_to_delete] = False
+    tables.edges.replace_with(tables.edges[edges_to_keep])
+
+    logger.debug("Update tables: sorting and indexing final tables.")
+    tables.sort()
+    tables.build_index()
+    tables.compute_mutation_parents()
+    return tables.tree_sequence()
+
+
+def coalesce_mutations(ts):
+    """
+    Examine all time-0 samples and their (full-sequence) sibs and create
+    new nodes to represent overlapping sets of mutations. The algorithm
+    is greedy and makes no guarantees about uniqueness or optimality.
+    Also note that we don't recurse and only reason about mutation sharing
+    at a single level in the tree.
+
+    Note: this function will most likely move to sc2ts once it has moved
+    over to tskit's hapotype matching engine.
+    """
+    # We depend on mutations having a time below.
+    assert np.all(np.logical_not(np.isnan(ts.mutations_time)))
+
+    tree = ts.first()
+
+    # Get the samples that span the whole sequence
+    samples = []
+    for u in ts.samples(time=0):
+        e = tree.edge(u)
+        assert e != -1
+        edge = ts.edge(e)
+        if edge.left == 0 and edge.right == ts.sequence_length:
+            samples.append(u)
+    logger.info(f"Coalescing mutations for {len(samples)} full-span samples")
+
+    # For each node in one of the sib groups, the set of mutations.
+    node_mutations = {}
+    for sample in samples:
+        u = tree.parent(sample)
+        for v in tree.children(u):
+            # Filter out non-tree like things. If the edge spans the whole genome
+            # then it must be present in the first tree.
+            edge = ts.edge(tree.edge(v))
+            assert edge.child == v and edge.parent == u
+            if edge.left == 0 and edge.right == ts.sequence_length:
+                if v not in node_mutations:
+                    node_mutations[v] = node_mutation_descriptors(ts, v)
+
+    # For each sample, what is the ("a" more accurately - this is greedy)
+    # maximum mutation overlap with one of its sibs?
+    max_sample_overlap = {}
+    for sample in samples:
+        u = tree.parent(sample)
+        max_overlap = set()
+        for v in tree.children(u):
+            if v != sample and v in node_mutations:
+                overlap = node_mutations[sample] & node_mutations[v]
+                if len(overlap) > len(max_overlap):
+                    max_overlap = overlap
+        max_sample_overlap[sample] = max_overlap
+
+    # Group the maximum mutation overlaps by the parent and mutation pattern
+    sib_groups = collections.defaultdict(set)
+    # Make sure we don't use the same node in more than one sib-set
+    used_nodes = set()
+    for sample in samples:
+        u = tree.parent(sample)
+        sample_overlap = frozenset(max_sample_overlap[sample])
+        key = (u, sample_overlap)
+        if len(sample_overlap) > 0:
+            for v in tree.children(u):
+                if v in node_mutations and v not in used_nodes:
+                    if sample_overlap.issubset(node_mutations[v]):
+                        sib_groups[key].add(v)
+                        used_nodes.add(v)
+        # Avoid creating a new node when there's only one node in the sib group
+        if len(sib_groups[key]) < 2:
+            del sib_groups[key]
+
+    mutations_to_delete = []
+    edges_to_delete = []
+    for (_, overlap), sibs in sib_groups.items():
+        for mut_desc in overlap:
+            for sib in sibs:
+                condition = np.logical_and(
+                    ts.mutations_node == sib,
+                    ts.mutations_site == mut_desc.site,
+                    ts.mutations_parent == mut_desc.parent,
+                )
+                mutations_to_delete.extend(np.where(condition)[0])
+                edges_to_delete.append(tree.edge(sib))
+
+    tables = ts.dump_tables()
+    for (parent, overlap), sibs in sib_groups.items():
+        group_parent = len(tables.nodes)
+        tables.edges.add_row(0, ts.sequence_length, parent, group_parent)
+        max_sib_time = 0
+        for sib in sibs:
+            max_sib_time = max(max_sib_time, ts.nodes_time[sib])
+            tables.edges.add_row(0, ts.sequence_length, group_parent, sib)
+        parent_time = ts.nodes_time[parent]
+        assert max_sib_time < parent_time
+        diff = parent_time - max_sib_time
+        group_parent_time = max_sib_time + diff / 2
+        assert group_parent_time < parent_time
+
+        tables.nodes.add_row(
+            flags=constants.NODE_IS_IDENTICAL_SAMPLE_ANCESTOR,
+            time=group_parent_time,
+        )
+        # TODO would be good to store some provenance here about what
+        # motivated the creation of this node.
+        # metadata={"mutations": overlap})
+        for mut_desc in overlap:
+            tables.mutations.add_row(
+                site=mut_desc.site,
+                derived_state=mut_desc.derived_state,
+                node=group_parent,
+                time=group_parent_time,
+            )
+
+    num_del_mutations = len(mutations_to_delete)
+    num_new_nodes = len(tables.nodes) - ts.num_nodes
+    logger.info(
+        f"Coalescing mutation: delete {num_del_mutations} mutations; "
+        f"add {num_new_nodes} new nodes"
+    )
+    return update_tables(tables, edges_to_delete, mutations_to_delete)
+
+
+def push_up_reversions(ts):
+    # We depend on mutations having a time below.
+    assert np.all(np.logical_not(np.isnan(ts.mutations_time)))
+
+    tree = ts.first()
+    mutations_per_node = np.bincount(ts.mutations_node, minlength=ts.num_nodes)
+
+    # First get all the time-0 samples that have mutations, or the unique parents
+    # of those that do not.
+    samples = set()
+    for u in ts.samples(time=0):
+        if mutations_per_node[u] == 0:
+            u = tree.parent(u)
+            if ts.nodes_flags[u] == constants.NODE_IS_IDENTICAL_SAMPLE_ANCESTOR:
+                # Not strictly a sample, but represents some time-0 samples
+                samples.add(u)
+        else:
+            samples.add(u)
+
+    # Get the samples that span the whole sequence and also have
+    # parents that span the full sequence. No reason we couldn't
+    # update the algorithm to work with partial edges, it's just easier
+    # this way and it covers the vast majority of simple reversions
+    # that we see
+    full_span_samples = []
+    for u in samples:
+        parent = tree.parent(u)
+        assert parent != -1
+        full_edge = True
+        for v in [u, parent]:
+            assert v != -1
+            e = tree.edge(v)
+            if e == -1:
+                # The parent is the root
+                full_edge = False
+                break
+            edge = ts.edge(e)
+            if edge.left != 0 or edge.right != ts.sequence_length:
+                full_edge = False
+                break
+        if full_edge:
+            full_span_samples.append(u)
+
+    logger.info(f"Pushing reversions for {len(full_span_samples)} full-span samples")
+
+    # For each node check if it has an immediate reversion
+    sib_groups = collections.defaultdict(list)
+    for child in full_span_samples:
+        parent = tree.parent(child)
+        child_muts = {desc.site: desc for desc in node_mutation_descriptors(ts, child)}
+        parent_muts = {
+            desc.site: desc for desc in node_mutation_descriptors(ts, parent)
+        }
+        reversions = []
+        for site in child_muts:
+            if site in parent_muts:
+                if child_muts[site].is_reversion_of(parent_muts[site]):
+                    reversions.append((site, child))
+        # Pick the maximum set of reversions per sib group so that we're not
+        # trying to resolve incompatible reversion sets.
+        if len(reversions) > len(sib_groups[parent]):
+            sib_groups[parent] = reversions
+
+    tables = ts.dump_tables()
+    edges_to_delete = []
+    mutations_to_delete = []
+    for parent, reversions in sib_groups.items():
+        if len(reversions) == 0:
+            continue
+
+        sample = reversions[0][1]
+        assert all(x[1] == sample for x in reversions)
+        sites = [x[0] for x in reversions]
+        # Remove the edges above the sample and its parent
+        edges_to_delete.extend([tree.edge(sample), tree.edge(parent)])
+        # Create new node that is fractionally older than the current
+        # parent that will be the parent of both nodes.
+        grandparent = tree.parent(parent)
+        # Arbitrarily make it 1/8 of the branch_length. Probably should
+        # make it proportional to the number of mutations or something.
+        eps = tree.branch_length(parent) * 0.125
+        w_time = tree.time(parent) + eps
+        w = tables.nodes.add_row(flags=1 << 22, time=w_time)
+        # Add new edges to join the sample and parent to w, and then
+        # w to the grandparent.
+        tables.edges.add_row(0, ts.sequence_length, parent=w, child=parent)
+        tables.edges.add_row(0, ts.sequence_length, parent=w, child=sample)
+        tables.edges.add_row(0, ts.sequence_length, parent=grandparent, child=w)
+
+        for site in sites:
+            # Delete the reversion mutations above the sample
+            muts = np.where(
+                np.logical_and(ts.mutations_node == sample, ts.mutations_site == site)
+            )[0]
+            assert len(muts) == 1
+            mutations_to_delete.extend(muts)
+        # Move any non-reversions mutations above the parent to the new node.
+        for mut in np.where(ts.mutations_node == parent)[0]:
+            row = tables.mutations[mut]
+            if row.site not in sites:
+                tables.mutations[mut] = row.replace(node=w, time=w_time)
+
+    num_del_mutations = len(mutations_to_delete)
+    num_new_nodes = len(tables.nodes) - ts.num_nodes
+    logger.info(
+        f"Push reversions: delete {num_del_mutations} mutations; "
+        f"add {num_new_nodes} new nodes"
+    )
+    return update_tables(tables, edges_to_delete, mutations_to_delete)
 
 
 def _parse_date(date):
