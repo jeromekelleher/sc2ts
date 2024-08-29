b93c80000feab97db5b6e49416f681b07ec3bbdb
Jerome Kelleher <jk@well.ox.ac.uk>
2024-08-29 16:04:26 +0100

    Reasonable tests for alignments module

--- tests/test_alignments.py
+++ tests/test_alignments.py
@@ -1,10 +1,65 @@
+import pathlib
+import shutil
+import gzip
+
 import numpy as np
 import pytest
 from numpy.testing import assert_array_equal
 
-# FIXME - quick hacks here to get tests working
 from sc2ts import alignments as sa
 from sc2ts import core
+
+
+@pytest.fixture
+def data_cache():
+    cache_path = pathlib.Path("tests/data/cache")
+    if not cache_path.exists():
+        cache_path.mkdir()
+    return cache_path
+
+
+@pytest.fixture
+def alignments_fasta(data_cache):
+    cache_path = data_cache / "alignments.fasta"
+    if not cache_path.exists():
+        with gzip.open("tests/data/alignments.fasta.gz") as src:
+            with open(cache_path, "wb") as dest:
+                shutil.copyfileobj(src, dest)
+    return cache_path
+
+
+@pytest.fixture
+def alignments_store(data_cache, alignments_fasta):
+    cache_path = data_cache / "alignments.db"
+    if not cache_path.exists():
+        with sa.AlignmentStore(cache_path, "a") as a:
+            fasta = core.FastaReader(alignments_fasta)
+            a.append(fasta, show_progress=False)
+    return sa.AlignmentStore(cache_path)
+
+
+class TestAlignmentsStore:
+    def test_info(self, alignments_store):
+        assert "contains" in str(alignments_store)
+
+    def test_len(self, alignments_store):
+        assert len(alignments_store) == 55
+
+    def test_fetch_known(self, alignments_store):
+        a = alignments_store["SRR11772659"]
+        assert a.shape == (core.REFERENCE_SEQUENCE_LENGTH,)
+        assert a[0] == "X"
+        assert a[1] == "N"
+        assert a[-1] == "N"
+
+    def test_keys(self, alignments_store):
+        keys = list(alignments_store.keys())
+        assert len(keys) == len(alignments_store)
+        assert "SRR11772659" in keys
+
+    def test_in(self, alignments_store):
+        assert "SRR11772659" in alignments_store
+        assert "NOT_IN_STORE" not in alignments_store
 
 
 def test_get_gene_coordinates():
@@ -66,6 +121,12 @@ class TestEncodeAligment:
         with pytest.raises(ValueError):
             sa.decode_alignment(np.array(a))
 
+    def test_encode_real(self, alignments_store):
+        h = alignments_store["SRR11772659"]
+        a = sa.encode_alignment(h)
+        assert a[0] == -1
+        assert a[-1] == -1
+
 
 class TestMasking:
     # Window size of 1 is weird because we have to have two or more
@@ -113,3 +174,20 @@ class TestMasking:
         a = np.zeros(2, dtype=np.int8)
         with pytest.raises(ValueError):
             sa.mask_alignment(a, window_size=w)
+
+
+class TestEncodeAndMask:
+    def test_known(self, alignments_store):
+        a = alignments_store["SRR11772659"]
+        ma = sa.encode_and_mask(a)
+        assert ma.original_base_composition == {
+            "T": 9566,
+            "A": 8894,
+            "G": 5850,
+            "C": 5472,
+            "N": 121,
+        }
+        assert ma.original_md5 == "e96feaa72c4f4baba73c2e147ede7502"
+        assert len(ma.masked_sites) == 133
+        assert ma.masked_sites[0] == 1
+        assert ma.masked_sites[-1] == 29903
