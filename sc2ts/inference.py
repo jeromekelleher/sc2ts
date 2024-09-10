09ca451ed3a7603ebcd6d88db59b9246f971b292
Jerome Kelleher <jk@well.ox.ac.uk>
2024-09-10 13:20:19 +0100

    Move valiation to own module

--- sc2ts/inference.py
+++ sc2ts/inference.py
@@ -287,60 +287,6 @@ def increment_time(date, ts):
     return tables.tree_sequence()
 
 
-def _validate_samples(ts, samples, alignment_store, show_progress):
-    strains = [ts.node(u).metadata["strain"] for u in samples]
-    G = np.zeros((ts.num_sites, len(samples)), dtype=np.int8)
-    keep_sites = ts.sites_position.astype(int)
-    strains_iter = enumerate(strains)
-    with tqdm.tqdm(
-        strains_iter,
-        desc="Read",
-        total=len(strains),
-        position=1,
-        leave=False,
-        disable=not show_progress,
-    ) as bar:
-        for j, strain in bar:
-            ma = alignments.encode_and_mask(alignment_store[strain])
-            G[:, j] = ma.alignment[keep_sites]
-
-    vars_iter = ts.variants(samples=samples, alleles=tuple(core.ALLELES))
-    with tqdm.tqdm(
-        vars_iter,
-        desc="Check",
-        total=ts.num_sites,
-        position=1,
-        leave=False,
-        disable=not show_progress,
-    ) as bar:
-        for var in bar:
-            original = G[var.site.id]
-            non_missing = original != -1
-            if not np.all(var.genotypes[non_missing] == original[non_missing]):
-                raise ValueError("Data mismatch")
-
-
-def validate(ts, alignment_store, show_progress=False):
-    """
-    Check that all the samples in the specified tree sequence are correctly
-    representing the original alignments.
-    """
-    samples = ts.samples()[1:]
-    chunk_size = 10**3
-    offset = 0
-    num_chunks = ts.num_samples // chunk_size
-    for chunk_index in tqdm.tqdm(
-        range(num_chunks), position=0, disable=not show_progress
-    ):
-        chunk = samples[offset : offset + chunk_size]
-        offset += chunk_size
-        _validate_samples(ts, chunk, alignment_store, show_progress)
-
-    if ts.num_samples % chunk_size != 0:
-        chunk = samples[offset:]
-        _validate_samples(ts, chunk, alignment_store, show_progress)
-
-
 @dataclasses.dataclass
 class Sample:
     strain: str
@@ -1274,6 +1220,28 @@ def convert_tsinfer_sample_data(ts, genotypes):
     return sd
 
 
+class TsinferProgressMonitor(tsinfer.progress.ProgressMonitor):
+    def __init__(self, desc, *args, **kwargs):
+        self.desc = desc
+        super().__init__(*args, **kwargs)
+
+    def get(self, key, total):
+        bar_format = (
+            "{desc}{percentage:3.0f}%|{bar}"
+            "| {n_fmt}/{total_fmt} [{elapsed}, {rate_fmt}{postfix}]"
+        )
+        self.current_instance = tqdm.tqdm(
+            desc=self.desc,
+            total=total,
+            disable=not self.enabled,
+            bar_format=bar_format,
+            dynamic_ncols=True,
+            smoothing=0.01,
+            unit_scale=True,
+        )
+        return self.current_instance
+
+
 def match_tsinfer(
     samples,
     ts,
@@ -1302,11 +1270,10 @@ def match_tsinfer(
         # Let's say a double break with 5 mutations is the most unlikely thing
         # we're interested in solving for exactly.
         likelihood_threshold = rho**2 * mu**5
-    pm = tsinfer.inference._get_progress_monitor(
+    # pm = tsinfer.inference._get_progress_monitor(
+    pm = TsinferProgressMonitor(
+        f"Match ({likelihood_threshold:.2g})",
         show_progress,
-        generate_ancestors=False,
-        match_ancestors=False,
-        match_samples=False,
     )
 
     # This is just working around tsinfer's input checking logic. The actual value
