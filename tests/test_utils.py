bfbe853206e56e8c3e000828f1bd5b2f03856384
Jerome Kelleher <jk@well.ox.ac.uk>
2024-09-17 17:03:36 +0100

    Fixup

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,7 @@ import util
 
 class TestPadSites:
     def check_site_padding(self, ts):
-        ts = sc2ts.utils.pad_sites(ts)
+        ts = sc2ts.pad_sites(ts)
         ref = sc2ts.core.get_reference_sequence(as_array=True)
         assert ts.num_sites == len(ref) - 1
         ancestral_state = ts.tables.sites.ancestral_state.view("S1").astype(str)
