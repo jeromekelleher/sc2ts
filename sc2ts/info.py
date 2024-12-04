7cf1468493ab57f855346f8c6d438af74e377f7d
Yan Wong <yan.wong@bdi.ox.ac.uk>
2024-12-04 16:48:40 +0000

    Correct md location in recombinant_samples_report

--- sc2ts/info.py
+++ sc2ts/info.py
@@ -1346,7 +1346,7 @@ class TreeInfo:
             closest_recombinant, path_length = self._get_closest_recombinant(tree, node)
             sample_is_recombinant = False
             if closest_recombinant != -1:
-                recomb_date = self.ts.node(closest_recombinant).metadata["date_added"]
+                recomb_date = self.ts.node(closest_recombinant).metadata["sc2ts"]["date_added"]
                 sample_is_recombinant = recomb_date == str(node_summary["date"])
             summary = {
                 "recombinant": closest_recombinant,
