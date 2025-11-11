eeadfe4a39e4cebf00a66c696ede4bbddba6ba87
Jerome Kelleher <jk@well.ox.ac.uk>
2025-11-11 16:22:23 +0000

    Update the pytest config to make fixtures optional

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,3 @@
-pytest_plugins = ["fixtures"]
+# We keep the fixtures in a separate modules so that the plugin can
+# be excluded when running tests on the bare API
+pytest_plugins = ["sc2ts_fixtures"]
